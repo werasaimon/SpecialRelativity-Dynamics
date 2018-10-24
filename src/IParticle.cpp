c518655108340b969ce5869fbc2b585469851640
werasaimon <nicemankit@gmail.com>
2018-10-25 00:16:43 +0300

    relativity-physics

--- src/IParticle.cpp
+++ src/IParticle.cpp
@@ -21,17 +21,6 @@ Vector4 IParticle::GetVelocity4() const
 void IParticle::ApplyImpulse(const Vector3 &impulse)
 {
     Vector3 impuls = impulse * mMassInverse;// /  gamma(mVelocity,mLightSpeed);
-
-   // impuls = Vector3::Clamp( impuls , mLightSpeed - 0.001  );
-
     mVelocity = VelocityAddition(mVelocity,impuls,Gamma(impuls/mLightSpeed),mLightSpeed);
-
-    //mVelocity = VelocityAddition2(mVelocity,impuls,Gamma(mVelocity/mLightSpeed),mLightSpeed);
-
-//    Vector4 V4(mVelocity,gamma_inv(mVelocity,mLightSpeed));
-//    std::cout << " L: " <<  V4.Length()  << "  V:" << mVelocity.Length() << std::endl;
-
-    std::cout<< mVelocity.Length() << std::endl;
-
 }
 
